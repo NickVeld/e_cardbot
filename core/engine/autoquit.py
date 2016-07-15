4a319f2d28bafac1eb7effa3a4106a706f4e6268
NickVeld <NickVeld@users.noreply.github.com>
2016-07-15 15:32:45 +0300

    fix autoquit_run

--- core/engine/autoquit.py
+++ core/engine/autoquit.py
@@ -1,5 +1,7 @@
 def autoquit_run(tapi):
     for user in tapi.db_shell.get_ready_for_autoquit():
+        pers_id = int(user['pers_id'])
         for worker in tapi.workers_list:
-            worker.quit(int(user['pers_id']), int(user['pers_id']), "Вы были неактивны долгое время.\n")
-        tapi.db_shell.modify_last_activity(user, True)
+            worker.quit(pers_id, pers_id, "Вы были неактивны долгое время.\n")
+        tapi.db_shell.modify_last_activity(pers_id, True)
+        tapi.db_shell.calculate_reminder_time(pers_id)
