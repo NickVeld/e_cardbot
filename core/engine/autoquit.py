29b60a58010a38627ff52b5ca76011ac33293688
NickVeld <NickVeld@users.noreply.github.com>
2016-07-15 03:49:09 +0300

    autoquit_run

--- core/engine/autoquit.py
+++ core/engine/autoquit.py
@@ -1,7 +1,7 @@
-from core.workers.workers import WorkersList
-
 def autoquit_run(tapi):
     for user in tapi.db_shell.get_ready_for_autoquit():
-        for worker in WorkersList.workers:
-            worker.quit(['user.pers_id'], user['pers_id'], "Вы были неактивны долгое время.")
-            tapi.db_shell.modify_last_activity(user, True)
+        for worker in tapi.workers_list:
+            print(type(worker).__name__ + ' finded')
+            worker.quit(user['pers_id'], user['pers_id'], "Вы были неактивны долгое время.")
+            print(type(worker).__name__ + ' checked')
+        tapi.db_shell.modify_last_activity(user, True)
