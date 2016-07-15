0a9dbd002583da079b2ef1b8508641543825be62
NickVeld <NickVeld@users.noreply.github.com>
2016-07-15 15:34:07 +0300

    fix reminder_run

--- core/engine/reminder.py
+++ core/engine/reminder.py
@@ -1,4 +1,3 @@
-import sys
 import datetime
 
 from ..workers.workers import WorkersList
@@ -10,13 +9,15 @@ def now_is_belong_to(period):
 
 def get_inline_text_keyboard():
     keyboard = []
-    for worker in WorkersList.workers:
+    for worker, worker_class in WorkersList.workers:
         if worker.endswith('Card') and not worker == 'HangCard':
-            keyboard.append([{'text': worker.replace("Card", " card")
-                                 , 'callback_data': getattr(sys.modules[__name__], str).COMMAND}])
+            keyboard.append([{'text': worker.replace("Card", " card"), 'callback_data': worker_class.COMMAND}])
     return keyboard
 
 def reminder_run(tapi):
-    for user in tapi.db.collection_names:
-        if now_is_belong_to(tapi.db[user]['activity'][datetime.datetime.utcnow().weekday]['reminder_time']):
-            tapi.send_inline_keyboard("Как насчет того, чтобы поучить карточки?", int(user), get_inline_text_keyboard())
+    for user_activity in tapi.db.collection_names():
+        if user_activity.endswith('activity'):
+            if now_is_belong_to(int(tapi.db[user_activity].find_one(
+                    {'weekday': str(datetime.datetime.utcnow().weekday())})['reminder_time'])):
+                tapi.send_inline_keyboard("Как насчет того, чтобы поучить карточки?"
+                                          , int(user_activity.replace('.activity', '', 1)), get_inline_text_keyboard())
