982bed75fa09fff13f60ef69d83e2b33fb9ff38b
NickVeld <NickVeld@users.noreply.github.com>
2016-07-15 03:50:47 +0300

    autoquit_run without prints

--- core/engine/autoquit.py
+++ core/engine/autoquit.py
@@ -1,7 +1,5 @@
 def autoquit_run(tapi):
     for user in tapi.db_shell.get_ready_for_autoquit():
         for worker in tapi.workers_list:
-            print(type(worker).__name__ + ' finded')
             worker.quit(user['pers_id'], user['pers_id'], "Вы были неактивны долгое время.")
-            print(type(worker).__name__ + ' checked')
         tapi.db_shell.modify_last_activity(user, True)
