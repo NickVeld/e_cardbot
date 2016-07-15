f9e6216bc46321ea1d721b8293e5d738eeaf669d
NickVeld <NickVeld@users.noreply.github.com>
2016-07-15 10:11:26 +0300

    fix autoquit ( int(id) )

--- core/engine/autoquit.py
+++ core/engine/autoquit.py
@@ -1,5 +1,5 @@
 def autoquit_run(tapi):
     for user in tapi.db_shell.get_ready_for_autoquit():
         for worker in tapi.workers_list:
-            worker.quit(user['pers_id'], user['pers_id'], "Вы были неактивны долгое время.")
+            worker.quit(int(user['pers_id']), int(user['pers_id']), "Вы были неактивны долгое время.\n")
         tapi.db_shell.modify_last_activity(user, True)
